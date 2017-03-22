dcef24a8532cffc7dcf9372f13246144590b45ab
lotaku <317399510@qq.com>
2017-03-22 14:47:33 +0800

    update

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-
-
-
 echo "publish to lotaku.github.io"
-rm -rf /Users/l/learnspace/lotaku.github.io/*
+rm -y -rf /Users/l/learnspace/lotaku.github.io/*
 pelican -o /Users/l/learnspace/lotaku.github.io
+cd /Users/l/learnspace/lotaku.github.io
+git add .
+git commit -m "update"
+git push
+echo "Done"
